98e89db0e55c86a8571c92a26495369f661f5d59
vhristova912 <viki66hristova@gmail.com>
2023-02-06 09:18:53 +0200

    DogsProject Test

---
+++ DogsProject/DogsProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DogsProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
